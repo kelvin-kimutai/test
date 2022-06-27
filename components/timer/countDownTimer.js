d0c8e34aede12cab4af9c79aaaf0307f558c56ec
kelvin-kimutai <kelvinkpkrr@gmail.com>
2022-06-27 12:29:53 +0300

    Added session expiry

--- components/timer/countDownTimer.js
+++ components/timer/countDownTimer.js
@@ -29,8 +29,8 @@ export default function CountDownTimer() {
       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       if (distance < 0) {
-        // router.replace("/session-expired");
-        // clearInterval(interval.current);
+        router.replace("/session-expired");
+        clearInterval(interval.current);
       } else {
         setTimerDays(("0" + days).slice(-2));
         setTimerHours(("0" + hours).slice(-2));
